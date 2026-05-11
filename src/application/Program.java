3789cdd2653357b9a00f99344b4da00f82f45661
Gabriel Andrade <andradeoliveirabiel@gmail.com>
2026-05-11 11:05:21 -0300

    color changes and bug fixes

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		while(true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -25,6 +25,8 @@ public class Program {
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				
+				
 				System.out.println();
 				System.out.print("Target : ");
 				ChessPosition target = UI.readChessPosition(sc);
